27f2fb22036c3c0065b74d60f2564760022e4207
RookieJoel <jojackchanah@gmail.com>
2025-05-28 14:28:51 +0700

    add-responsive-Header

--- shop-web-design/src/components/Header.svelte
+++ shop-web-design/src/components/Header.svelte
@@ -1,10 +1,9 @@
-
 <script lang="ts">
-  export let title:string = "Well, this is my mini-project on designing a shop website. and yeah it's just a design :)";
+  export let title: string = "Well, this is my mini-project on designing a shop website. and yeah it's just a design :)";
 </script>
 
 <header class="w-full bg-black text-white">
-  <div class="max-w-screen-xl mx-auto p-1.5 text-center text-l font-bold">
+  <div class="max-w-screen-xl mx-auto px-4 py-2 text-center text-base sm:text-lg md:text-xl font-bold break-words">
     {title}
   </div>
 </header>
--- shop-web-design/src/components/Nav.svelte
+++ shop-web-design/src/components/Nav.svelte
@@ -1,20 +1,28 @@
 <nav>
-    <div class ="flex justify-between items-center bg-gray-800 p-4">
-        <div class="text-white text-2xl font-extrabold">
-            <a href="/">ShopShap</a>    
-        </div>
-        <div class="flex space-x-4 pl-2">
-            <a href="/" class="text-white hover:text-gray-300">Home</a>
-            <a href="/products" class="text-white hover:text-gray-300">Products</a>
-            <a href="/about" class="text-white hover:text-gray-300">About</a>
-            <a href="/contact" class="text-white hover:text-gray-300">Contact</a>
-        </div>
-        <div class="flex items-center space-x-4">
-            <a href="/cart" class="text-white hover:text-gray-300">Cart</a>
-            <a href="/login" class="text-white hover:text-gray-300">Login</a>
-            <a href="/signup" class="text-white hover:text-gray-300">Sign Up</a>    
-        </div>
+  <div class="flex justify-between items-center bg-gray-800 p-4">
+    <div class="flex items-center space-x-2">
+      <button class="md:hidden text-white focus:outline-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M4 18q-.425 0-.712-.288T3 17t.288-.712T4 16h16q.425 0 .713.288T21 17t-.288.713T20 18zm0-5q-.425 0-.712-.288T3 12t.288-.712T4 11h16q.425 0 .713.288T21 12t-.288.713T20 13zm0-5q-.425 0-.712-.288T3 7t.288-.712T4 6h16q.425 0 .713.288T21 7t-.288.713T20 8z"/>
+        </svg>
+      </button>
+      
+      <div class="text-white text-2xl font-extrabold md:text-center">
+        <a href="/">ShopShap</a>
+      </div>
     </div>
 
-</nav>
+    <div class="hidden md:flex space-x-4 pl-2">
+      <a href="/" class="text-white hover:text-gray-300">Home</a>
+      <a href="/products" class="text-white hover:text-gray-300">Products</a>
+      <a href="/about" class="text-white hover:text-gray-300">About</a>
+      <a href="/contact" class="text-white hover:text-gray-300">Contact</a>
+    </div>
 
+    <div class="flex items-center space-x-4">
+      <a href="/cart" class="text-white hover:text-gray-300">Cart</a>
+      <a href="/login" class="text-white hover:text-gray-300">Login</a>
+      <a href="/signup" class="text-white hover:text-gray-300">Sign Up</a>
+    </div>
+  </div>
+</nav>
