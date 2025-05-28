83bb7872115fc2fecfabf5378b0b1acbb9b7f1a7
RookieJoel <jojackchanah@gmail.com>
2025-05-28 15:08:15 +0700

    add-drawer

--- shop-web-design/src/components/Nav.svelte
+++ shop-web-design/src/components/Nav.svelte
@@ -1,12 +1,38 @@
 <nav>
   <div class="flex justify-between items-center bg-gray-800 p-4">
     <div class="flex items-center space-x-2">
-      <button class="md:hidden text-white focus:outline-none">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M4 18q-.425 0-.712-.288T3 17t.288-.712T4 16h16q.425 0 .713.288T21 17t-.288.713T20 18zm0-5q-.425 0-.712-.288T3 12t.288-.712T4 11h16q.425 0 .713.288T21 12t-.288.713T20 13zm0-5q-.425 0-.712-.288T3 7t.288-.712T4 6h16q.425 0 .713.288T21 7t-.288.713T20 8z"/>
-        </svg>
-      </button>
-      
+      <div class="drawer md:hidden">
+        <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+          <label for="my-drawer" class="btn btn-primary drawer-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M4 18q-.425 0-.712-.288T3 17t.288-.712T4 16h16q.425 0 .713.288T21 17t-.288.713T20 18zm0-5q-.425 0-.712-.288T3 12t.288-.712T4 11h16q.425 0 .713.288T21 12t-.288.713T20 13zm0-5q-.425 0-.712-.288T3 7t.288-.712T4 6h16q.425 0 .713.288T21 7t-.288.713T20 8z"
+              />
+            </svg>
+          </label>
+        </div>
+        <div class="drawer-side">
+          <label
+            for="my-drawer"
+            aria-label="close sidebar"
+            class="drawer-overlay"
+          ></label>
+          <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            <li><a>Sidebar Item 1</a></li>
+            <li><a>Sidebar Item 2</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <button class="md:hidden text-white focus:outline-none"> </button>
+
       <div class="text-white text-2xl font-extrabold md:text-center">
         <a href="/">ShopShap</a>
       </div>
